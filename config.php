f02043d58b32ea17ad07812211c8d695c95d37ac
Pasan Kalhara <76808092+Kalharapasan@users.noreply.github.com>
2025-10-16 15:34:03 +0530

    Create index.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
 define('DB_PASS', '');
-define('DB_NAME', 'payroll_system');
+define('DB_NAME', 'psII');
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
